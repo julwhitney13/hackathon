ea6b80aadad56c4fc96806690bab4e1bef6a666c
Campbell Alden <alden.ca@husky.neu.edu>
2017-05-06 01:09:00 -0700

    Some client code

--- public/client.js
+++ public/client.js
@@ -1,54 +1,97 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var mouse = {
-        click: false,
-        move: false,
-        pos: {x:0, y:0},
-        pos_prev: false
-    };
+function draw_circle(canvas, x, y) {
+    canvas.beginPath();
+    canvas.arc(x, y, 5, 0, 2 * Math.PI, false);
+    canvas.closePath()
+    canvas.fillStyle = 'green';
+    canvas.fill();
+    canvas.lineWidth = 5;
+    canvas.strokeStyle = '#003300';
+    canvas.stroke();
+}
 
+var keys = {
+    left: false,
+    right: false,
+    up: false,
+    down: false
+};
+
+document.addEventListener("onkeydown", function(e) {
+    switch (e.keyCode) {
+        case 37:
+            keys.left = true;
+            break;
+        case 38:
+            keys.up = true;
+            break;
+        case 39:
+            key.right = true;
+            break;
+        case 40:
+            key.down = true;
+    }
+});
+
+document.addEventListener("onkeyup", function(e) {
+    switch (e.keyCode) {
+        case 37:
+            keys.left = false;
+            break;
+        case 38:
+            keys.up = false;
+            break;
+        case 39:
+            key.right = false;
+            break;
+        case 40:
+            key.down = false;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     // get canvas element and create context
-    var canvas  = document.getElementById('drawing');
+    var canvas  = document.getElementById('game');
     var context = canvas.getContext('2d');
-    var width   = window.innerWidth;
-    var height  = window.innerHeight;
+    var width   = 2000;
+    var height  = 2000;
     var socket  = io.connect();
 
     // set canvas to full browser width/height
     canvas.width = width;
     canvas.height = height;
 
-    // register mouse event handlers
-    canvas.onmousedown = function(e){ mouse.click = true; };
-    canvas.onmouseup = function(e){ mouse.click = false; };
-
-    canvas.onmousemove = function(e) {
-        // normalize mouse position to range 0.0 - 1.0
-        mouse.pos.x = e.clientX / width;
-        mouse.pos.y = e.clientY / height;
-        mouse.move = true;
+    var character = {
+        id: false,
+        pos: false
     };
 
     // draw line received from server
-    socket.on('draw_line', function (data) {
-    var line = data.line;
-        context.beginPath();
-        context.moveTo(line[0].x * width, line[0].y * height);
-        context.lineTo(line[1].x * width, line[1].y * height);
-        context.stroke();
+    socket.on('update_characters', function (all_characters) {
+        context.clearRect(0, 0, canvas.width, canvas.height); // Clear out the canvas
+        for (var i in all_characters) {
+            draw_circle(context, all_characters[i].x, all_characters[i].y);
+        }
     });
 
     // main loop, running every 25ms
     function mainLoop() {
-        // check if the user is drawing
-        if (mouse.click && mouse.move && mouse.pos_prev) {
-            // send line to to the server
-            socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-            mouse.move = false;
-        }
+        // Calculate new location
+        dx = keys.left ? -1 : 0;
+        dx += keys.right ? 1 : 0;
 
-        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+        dy = keys.down ? -1 : 0;
+        dy += keys.up ? 1 : 0;
+
+        character.pos.x += dx;
+        character.pos.y += dy;
+
+        socket.emit('move_character');
         setTimeout(mainLoop, 25);
-
     }
-    mainLoop();
+
+    socket.on('init_character', function(data) {
+        character.id = data.id;
+        character.pos = data.position;
+        mainLoop();
+    });
 });
