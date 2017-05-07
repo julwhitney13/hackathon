95fe1ad09380c57ab7d8224b1bbad14ad22117cb
Eddie Hou <dearestatut@hotmail.com>
2017-05-07 15:06:51 -0700

    specials added and working

--- public/client.js
+++ public/client.js
@@ -205,6 +205,10 @@ function characterAngle(character) {
     return pointToAngle(mouseX, mouseY, character.pos.x, character.pos.y);
 }
 
+function moveBackground(canvas, viewOrigin) {
+  canvas.style.backgroundPosition = '-' + viewOrigin.left.toString() + 'px -' + viewOrigin.top.toString() + 'px';
+}
+
 function resetGame() {
   var setupDiv = document.getElementById('setupDiv');
 
@@ -234,8 +238,10 @@ function loadGame() {
         move_to:{x:0,y:0},
         move: false,
         id: false,
-        pos: {x:0, y:0, angle: 0}
+        pos: {x:0, y:0, angle: 0, special:false}
     };
+
+    var specials = {};
 
     var viewOrigin = {
         top:0,
@@ -271,6 +277,11 @@ function loadGame() {
         var attack_position = {x: attackX+viewOrigin.left, y: attackY+viewOrigin.top};
 
         var attack = {id: character.id, attack: attack_position, type: 'A'};
+        if (character.pos.special) {
+            console.log("SPECIAL ATTACK");
+            attack.type = 'B';
+            character.pos.special = false;
+        }
         socket.emit('attack',attack);
         document.getElementById('attack_sound').play();
     };
@@ -278,7 +289,7 @@ function loadGame() {
     // draw line received from server
     socket.on('update_characters', function (all_characters) {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        canvas.style.backgroundPosition = '-' + viewOrigin.left.toString() + 'px -' + viewOrigin.top.toString() + 'px';
+        moveBackground(canvas,viewOrigin);
         context.rect(0, 0, canvas.width, canvas.height);
         context.fillStyle = 'rgba(47, 79, 79, 0.80)';
         context.fill();
@@ -299,6 +310,14 @@ function loadGame() {
               scoreBoard.innerHTML = all_characters[i].score.toString();
             }
         }
+        for (var j in specials) {
+            special = specials[j];
+            special.x -= viewOrigin.left;
+            special.y -= viewOrigin.top;
+            if (point_in_range(character, special.x, special.y)) {
+              draw_circle(context, special.x, special.y);
+            }
+        }
     });
 
     socket.on('character_died', function () {
@@ -310,8 +329,14 @@ function loadGame() {
         document.getElementById('death_sound').play();
     });
 
+    socket.on('got_special', function () {
+        console.log("got_special");
+        character.pos.special = true;
+    });
+
     // main loop, running every 25ms
     function mainLoop() {
+
         if (character.move) {
             move_character_towards_cursor(character,character.move_to.x,character.move_to.y, viewOrigin);
         }
@@ -321,7 +346,7 @@ function loadGame() {
                 move_to:{x:character.move_to.x,y:character.move_to.y},
                 move: character.move,
                 id: character.id,
-                pos: {x:character.pos.x+viewOrigin.left, y:character.pos.y+viewOrigin.top, angle: character.pos.angle}
+                pos: {x:character.pos.x+viewOrigin.left, y:character.pos.y+viewOrigin.top, angle: character.pos.angle, special:character.pos.special}
             };
             socket.emit('move_character', correctedCharacter);
         }
@@ -331,11 +356,39 @@ function loadGame() {
         }
     }
 
+// 1165 // 226
+    socket.on('update_specials', function(data) {
+        specials = data;
+    });
+
     socket.on('init_character', function(data) {
         character.id = data.id;
-        character.pos.x = data.pos.x;
-        character.pos.y = data.pos.y;
+        if (data.pos.x < width/2) {
+          character.pos.x = data.pos.x;
+        }
+        else if ((data.pos.x >= width/2) && (data.pos.x < (width*3)/2)) {
+          character.pos.x = width/2;
+          viewOrigin.left = data.pos.x - width/2;
+        }
+        else if (data.pos.x >= (width*3)/2) {
+          viewOrigin.left = mapWidth - width;
+          character.pos.x = data.pos.x - viewOrigin.left;
+        }
+
+        if (data.pos.y < height/2) {
+          character.pos.y = data.pos.y;
+        }
+        else if ((data.pos.y >= height/2) && (data.pos.y < (height*3)/2)) {
+          character.pos.y = height/2;
+          viewOrigin.top = data.pos.y - height/2;
+        }
+        else if (data.pos.y >= (height*3)/2) {
+          viewOrigin.top = mapHeight - height;
+          character.pos.y = data.pos.y - viewOrigin.top;
+        }
+
         character.pos.angle = data.pos.angle;
+        moveBackground(canvas, viewOrigin);
         document.getElementById('enter_sound').play();
         mainLoop();
     });
